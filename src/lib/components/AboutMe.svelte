b29f61621ac6680b0208affe75b085acc18db9d4
luanduongtel4vn <hoangluan@tel4vn.com>
2025-02-04 11:56:16 +0700

    feat(docs): update cv pdf, about me

--- src/lib/components/AboutMe.svelte
+++ src/lib/components/AboutMe.svelte
@@ -1,12 +1,11 @@
 <p>
-    I'm <span class="fw-bold">Luan</span>, an experienced full-stack developer specializing in a range of technologies.
-    I am currently working on developing solutions in the areas of contact center and call
-    center functionality as well as API microservices, streaming platforms and their
-    integration with machine learning applications.
+	Backend Software Engineer with <span class="fw-bold">more than 4 years of experience</span> in
+	building scalable, high performance for
+	<span class="fw-bold">CallCenter, CRM and integrated softwares</span>, processing
+	<span class="fw-bold">several million events per day</span>.
 </p>
 <p>
-    My career has been characterized by a tireless pursuit of improving my skills. I
-    actively work on mastering new technologies like Svelte, JavaScript, Docker,
-    Continuous Integration/Continuous Deployment (CI/CD) practices, and Jenkins to improve
-    my skills as a full-stack developer.
+	I am currently pursuing a <span class="fw-bold">Master's degree in Software Engineering</span>,
+	with the goal of deepening my knowledge and expertise in the fields of
+	<span class="fw-bold">Artificial Intelligence, Machine Learning, and Blockchain technology</span>.
 </p>
